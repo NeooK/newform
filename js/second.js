dacf34f70986d3489a63741c8c539c7e918279cd
Ivan <84870990+NeooK@users.noreply.github.com>
2023-07-23 12:51:49 +0300

    anima

--- js/second.js
+++ js/second.js
@@ -2,10 +2,11 @@ function Reload() {
     var button = document.querySelector('.reload-button');
     button.classList.add('clicked');
 
-    document.querySelector('#rAn').classList.remove('hide');
+    document.querySelector('#loader-1').classList.remove('hide');
     setTimeout(function () {
         location.reload();
     }, 3000);
+
 }
 
 window.onload = function () {
